e77d5f14dcadd3ac40d29e6c8bc126bbe82dffc3
student <student@s218-pc12>
2021-11-19 12:48:26 +0100

    Dodaj pliki projektów.

---
+++ SortTest/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace SortTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testBtn = new System.Windows.Forms.Button();
+            this.currentSort = new System.Windows.Forms.Label();
+            this.bubbleSort = new System.Windows.Forms.Button();
+            this.doubleBubbleSort = new System.Windows.Forms.Button();
+            this.fastSort = new System.Windows.Forms.Button();
+            this.instertSort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // testBtn
+            // 
+            this.testBtn.Location = new System.Drawing.Point(713, 12);
+            this.testBtn.Name = "testBtn";
+            this.testBtn.Size = new System.Drawing.Size(75, 23);
+            this.testBtn.TabIndex = 0;
+            this.testBtn.Text = "Test";
+            this.testBtn.UseVisualStyleBackColor = true;
+            // 
+            // currentSort
+            // 
+            this.currentSort.AutoSize = true;
+            this.currentSort.Location = new System.Drawing.Point(643, 16);
+            this.currentSort.Name = "currentSort";
+            this.currentSort.Size = new System.Drawing.Size(64, 15);
+            this.currentSort.TabIndex = 1;
+            this.currentSort.Text = "Not Found";
+            // 
+            // bubbleSort
+            // 
+            this.bubbleSort.Location = new System.Drawing.Point(12, 43);
+            this.bubbleSort.Name = "bubbleSort";
+            this.bubbleSort.Size = new System.Drawing.Size(200, 25);
+            this.bubbleSort.TabIndex = 2;
+            this.bubbleSort.Text = "Bąbelkowe";
+            this.bubbleSort.UseVisualStyleBackColor = true;
+            // 
+            // doubleBubbleSort
+            // 
+            this.doubleBubbleSort.Location = new System.Drawing.Point(12, 74);
+            this.doubleBubbleSort.Name = "doubleBubbleSort";
+            this.doubleBubbleSort.Size = new System.Drawing.Size(200, 25);
+            this.doubleBubbleSort.TabIndex = 3;
+            this.doubleBubbleSort.Text = "Bąbelkowe Dwukierunkowe";
+            this.doubleBubbleSort.UseVisualStyleBackColor = true;
+            // 
+            // fastSort
+            // 
+            this.fastSort.Location = new System.Drawing.Point(12, 105);
+            this.fastSort.Name = "fastSort";
+            this.fastSort.Size = new System.Drawing.Size(200, 25);
+            this.fastSort.TabIndex = 4;
+            this.fastSort.Text = "Szybkie";
+            this.fastSort.UseVisualStyleBackColor = true;
+            // 
+            // instertSort
+            // 
+            this.instertSort.Location = new System.Drawing.Point(12, 12);
+            this.instertSort.Name = "instertSort";
+            this.instertSort.Size = new System.Drawing.Size(200, 25);
+            this.instertSort.TabIndex = 5;
+            this.instertSort.Text = "Przez Wstawianie";
+            this.instertSort.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.instertSort);
+            this.Controls.Add(this.fastSort);
+            this.Controls.Add(this.doubleBubbleSort);
+            this.Controls.Add(this.bubbleSort);
+            this.Controls.Add(this.currentSort);
+            this.Controls.Add(this.testBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button testBtn;
+        private System.Windows.Forms.Label currentSort;
+        private System.Windows.Forms.Button bubbleSort;
+        private System.Windows.Forms.Button doubleBubbleSort;
+        private System.Windows.Forms.Button fastSort;
+        private System.Windows.Forms.Button instertSort;
+    }
+}
+
