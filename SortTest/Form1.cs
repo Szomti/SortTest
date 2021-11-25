f31d4fb853757952828ba81244a14edc7941c5a3
Szomti <piotrpiotr1285@gmail.com>
2021-11-25 19:37:31 +0100

    Added: - Random - Working insertSort test

--- SortTest/Form1.cs
+++ SortTest/Form1.cs
@@ -45,6 +45,40 @@ namespace SortTest
             sortType = "fast";
         }
 
+        private readonly int amountOfNumbers = 10000;
+
+        internal static class RandomNumbers
+        {
+            private static System.Random r;
+
+            public static int NextNumber()
+            {
+                if (r == null)
+                    Seed();
+
+                return r.Next();
+            }
+
+            public static int NextNumber(int ceiling)
+            {
+                if (r == null)
+                    Seed();
+
+                return r.Next(ceiling);
+            }
+
+            public static void Seed()
+            {
+                r = new System.Random();
+            }
+
+            public static void Seed(int seed)
+            {
+                r = new System.Random(seed);
+            }
+        }
+
+
         private void testBtn_Click(object sender, EventArgs e)
         {
             var stopwatch = new Stopwatch();
@@ -52,10 +86,56 @@ namespace SortTest
             //Random here//
             ///////////////
             stopwatch.Start();
+            if (sortType=="insert") {
+                int[] d = new int[amountOfNumbers];
+                int i;
+                int j;
+                int x;
+
+                Console.Write(" Sortowanie przez wstawianie\n" + "-----------------------------\n" + "   (C)2005  Jerzy Walaszek\n\n" + "Przed sortowaniem:\n\n");
+
+                // Najpierw wypełniamy tablicę d[] liczbami pseudolosowymi
+                // a następnie wyświetlamy jej zawartość
+
+                RandomNumbers.Seed();
+
+                for (i = 0; i < amountOfNumbers; i++)
+                {
+                    d[i] = RandomNumbers.NextNumber() % 100;
+                }
+                for (i = 0; i < amountOfNumbers; i++)
+                {
+                    Console.Write("{0,4}", d[i]);
+                }
+                Console.Write("{0,4}", "\n");
+
+                // Sortujemy
+
+                for (j = amountOfNumbers - 2; j >= 0; j--)
+                {
+                    x = d[j];
+                    i = j + 1;
+                    while ((i < amountOfNumbers) && (x > d[i]))
+                    {
+                        d[i - 1] = d[i];
+                        i++;
+                    }
+                    d[i - 1] = x;
+                }
+
+                // Wyświetlamy wynik sortowania
+
+                Console.Write("{0,4}", "Po sortowaniu:\n\n");
+                for (i = 0; i < amountOfNumbers; i++)
+                {
+                    Console.Write("{0,4}", d[i]);
+                }
+                Console.Write("{0,4}", "\n");
+            }
             //////////////////////
             //Sorting types here//
             //////////////////////
-            Thread.Sleep(5000); // Delete this line later
+            //Thread.Sleep(5000); // Delete this line later
             stopwatch.Stop();
             sortingTime.Text = (stopwatch.ElapsedMilliseconds).ToString()+" ms";
         }
