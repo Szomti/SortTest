46295774d63d8822ac7b01ffeff50ab7467c25db
Szomti <piotrpiotr1285@gmail.com>
2021-11-26 22:39:18 +0100

    Changed: - More customizable .py file

--- randIntToFile.py
+++ randIntToFile.py
@@ -1,15 +1,18 @@
 from random import seed
 from random import randint
 fileName = "RandomNumbers" # Name your file
+fileExtension = "txt" # Extension of your file, for example "txt" is text file, while "py" is python file
+numberSplitter = "\n" # what will split the numbers, for example "\n" is next line, "\t" is tab, " " is space
 amountOfNumbers = 1000 # Choose the amount of numbers you want
 numberFrom = 1 # From which number it starts ( min number you can get )
 numberTo = 1000 # To which number you want it to be ( max number you can get )
 seed(1) # Not REAL random, so you need to choose a seed to get other values
-f = open(fileName+".txt","w+")
+
+f = open(fileName+"."+fileExtension,"w+")
 for numbers in range(amountOfNumbers):
     value = randint(numberFrom, numberTo)
     if(numbers<(amountOfNumbers-1)):
-        f.write(str(value)+"\n")
+        f.write(str(value)+numberSplitter)
     else:
         f.write(str(value))
 f.close()
