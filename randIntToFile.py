bb9b7fd9b49557fa477c9308873c3288ccccfb92
Szomti <piotrpiotr1285@gmail.com>
2021-11-26 22:05:06 +0100

    Fixed: - Now created file ( using .py app ) won't have empty line at the end

--- randIntToFile.py
+++ randIntToFile.py
@@ -4,9 +4,12 @@ fileName = "RandomNumbers" # Name your file
 amountOfNumbers = 1000 # Choose the amount of numbers you want
 numberFrom = 1 # From which number it starts ( min number you can get )
 numberTo = 1000 # To which number you want it to be ( max number you can get )
-seed(1) # Not REAL random, so you need to choose a seed
-f= open(fileName+".txt","w+")
-for _ in range(amountOfNumbers):
+seed(1) # Not REAL random, so you need to choose a seed to get other values
+f = open(fileName+".txt","w+")
+for numbers in range(amountOfNumbers):
     value = randint(numberFrom, numberTo)
-    f.write(str(value)+"\n")
+    if(numbers<(amountOfNumbers-1)):
+        f.write(str(value)+"\n")
+    else:
+        f.write(str(value))
 f.close()
